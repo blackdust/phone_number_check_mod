f62a28e721268fbfbcabf0a1e424311a0bcbad72
libochao <522427432@qq.com>
2015-09-18 16:16:16 +0800

    - -

--- app/controllers/phone_number_check_mod/application_controller.rb
+++ app/controllers/phone_number_check_mod/application_controller.rb
@@ -2,7 +2,8 @@ module PhoneNumberCheckMod
   class ApplicationController < ActionController::Base
     layout "phone_number_check_mod/application"
 # 1111222
-# 
+# 1111222
+# 333322
     if defined? PlayAuth
       helper PlayAuth::SessionsHelper
     end
